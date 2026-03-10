467f2a97e862cf1dc336dbcf94cb5aaa41f84933
a895411690 <47031748+a895411690@users.noreply.github.com>
2026-03-10 10:19:19 +0800

    add app/forms.py

---
+++ app/forms.py
@@ -0,0 +1,47 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from app.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('确认密码', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('注册')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('该用户名已被注册')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('该邮箱已被注册')
+
+class LoginForm(FlaskForm):
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember = BooleanField('记住我')
+    submit = SubmitField('登录')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    submit = SubmitField('更新')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('该用户名已被注册')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('该邮箱已被注册')
+
+class PostForm(FlaskForm):
+    title = StringField('标题', validators=[DataRequired()])
+    content = TextAreaField('内容', validators=[DataRequired()])
+    submit = SubmitField('发布')
